de22cfdfd294679ba94d2c243c231318c7e12207
Helena Freire <helenafreire97@gmail.com>
2020-05-31 20:49:02 -0300

    size, alpha, color, ylimits

--- Ludi_compounds/R/Analysis_Ludi.R
+++ Ludi_compounds/R/Analysis_Ludi.R
@@ -11,8 +11,8 @@ axis.set <- ludi.compounds %>%
   group_by(comp) %>% 
   summarize(center = (max(comp) + min(comp)) / 2)
 
-ylim <- max(ludi.compounds$log2FoldChange)
-ymin <- min(ludi.compounds$log2FoldChange)
+ylim <- max(ludi.compounds$log2FoldChange+5)
+ymin <- min(ludi.compounds$log2FoldChange-5)
 sig <- 1
 sig2 <- -1
 zero <- 0
@@ -21,8 +21,8 @@ nComp <- nrow(ludi.compounds)
 axis.set
 
 manhplot <- ggplot(ludi.compounds.nsig, aes(x = comp, y = log2FoldChange)) +
-  geom_point(size = 1.2, alpha = 0.7, color = "grey40") +
-  geom_point(data = ludi.compounds.sig, size = 1.2, color = "firebrick3", alpha = 0.7, aes(x = comp, y = log2FoldChange)) +
+  geom_point(size = 1.7, alpha = 0.5, color = "darkslateblue") +
+  geom_point(data = ludi.compounds.sig, size = 1.7, color = "firebrick3", alpha = 0.5, aes(x = comp, y = log2FoldChange)) +
   #geom_hline(yintercept = sig, color = "lightgoldenrod1", linetype = "dashed", size = 0.2) + 
   #geom_hline(yintercept = sig2, color = "lightgoldenrod1", linetype = "dashed", size = 0.2) +
   geom_hline(yintercept = zero, color = "black", linetype = "solid", size = 0.2) + 
@@ -46,3 +46,4 @@ manhplot <- ggplot(ludi.compounds.nsig, aes(x = comp, y = log2FoldChange)) +
 manhplot
 
 ggsave("./plots/manhattan_plot_certo.png", manhplot)
+
