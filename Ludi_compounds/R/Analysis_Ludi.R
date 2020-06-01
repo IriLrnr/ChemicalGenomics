6e204f4a5c4e78e4fc03ab53514ebd80d46e5e09
Helena Freire <helenafreire97@gmail.com>
2020-06-01 11:22:56 -0300

    separate top10

--- Ludi_compounds/R/Analysis_Ludi.R
+++ Ludi_compounds/R/Analysis_Ludi.R
@@ -31,13 +31,13 @@ tick.sizes[(breaks%%2==0)] <- 1
 
 manhplot <- ggplot(ludi.compounds.nsig, aes(x = comp, y = log2FoldChange)) +
   geom_point(size = 1, alpha = 0.5, color = "darkslateblue") +
-  geom_point(data = ludi.compounds.sig, size = 1, color = "firebrick3", alpha = 0.5, aes(x = comp, y = log2FoldChange)) +
+      geom_point(data = ludi.compounds.sig, size = 1, color = "firebrick3", alpha = 0.5, aes(x = comp, y = log2FoldChange)) +
   #geom_hline(yintercept = sig, color = "lightgoldenrod1", linetype = "dashed", size = 0.2) + 
   #geom_hline(yintercept = sig2, color = "lightgoldenrod1", linetype = "dashed", size = 0.2) +
   geom_hline(yintercept = zero, color = "black", linetype = "solid", size = 0.2) + 
   scale_x_continuous(label = compounds, breaks = axis.set$center) +
   scale_y_continuous(expand = c(0,0), breaks = breaks, labels = labels, limits = c(ymin, ylim)) +
-  ggtitle("Log2 FoldChange by compound") +
+  #ggtitle("Log2 FoldChange by compound") +
   labs(y = "Efluxers                          Influxers") + 
   theme_bw() +
   theme(
@@ -48,11 +48,11 @@ manhplot <- ggplot(ludi.compounds.nsig, aes(x = comp, y = log2FoldChange)) +
     panel.grid.minor.x = element_blank(),
     axis.text.x = element_text(angle = 90, size = 8, vjust = 0.5, hjust=1),
     axis.title.y = element_text(color="grey48", size=10),
-    title = element_text(hjust = 0.5, color = "grey40", size = 10),
+    #title = element_text(hjust = 0.5, color = "grey40", size = 10),
     plot.margin = margin(t = 3, r = 5, b = 3, l = 3, unit = "pt")
   )
 
 manhplot
 
-ggsave("./plots/manhattan_plot_certo.png", manhplot, height = 6, width = 5)
+ggsave("./plots/manhattan_plot.png", manhplot, height = 6, width = 5)
 
