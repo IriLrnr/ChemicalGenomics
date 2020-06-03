8169c58f0bd4e8fbc05de508c089ad0ae740b758
Irina Lerner <iri.lerner@gmail.com>
2020-06-03 18:59:34 -0300

    Export compound.t10 to xlsx

--- R/doses.R
+++ R/doses.R
@@ -1,10 +1,10 @@
 # Load used libraries
-library(openxlsx)
+library(xlsx)
 
 # Source analysis to make the tables
 source("./R/analysis.R")
 
-# 
+# Read coumpound information from HipHop library
 compound.table <- read.csv("./data/raw/compound_table.csv", header = T, sep = ";")
 
 ###################### DIFFERENT DOSES ANALYSIS ############################
@@ -37,5 +37,22 @@ compound.t10<- merge(compound.table[,1:4], transpose.t10, by="Screen.ID")
 compound.t10 <- compound.t10[order(compound.t10$name, compound.t10$conc),]
 
 # Print with colors to xlsx for Bessie
+wb <- createWorkbook()
+#add a worksheet to the workbook
+addWorksheet(wb, "Compound_t10", gridLines = TRUE)
+# write my analysis into the worksheet of the workbook, 
+writeData(wb, "Compound_t10", compound.t10)
+# here search for the respective HEX color-code and assign a name to the style
+warm1Style <- createStyle(fontColour = "black", bgFill = "firebrick")
+
+for(i in 1:nrow(transporters)){
+  conditionalFormatting(wb, "Compound_t10", cols = 5:14,
+                        rows = 1:nrow(compound.t10), rule = paste("=", paste(transporters[i,1]), sep = ""), style = warm1Style,
+                        type = "contains")
+}
+saveWorkbook(wb, "./output/compounds_t10.xlsx", overwrite = TRUE)
+
+# account the condition where "Significantly Warmer" is contained in a cell,
+# then apply the respective style to it (in this case, warm1Style)
 #############################################################################
 
